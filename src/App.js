2c2250597c30e704141ee9c607ceec1d86fae3d8
Sameer-0 <sameershaikhx123@gmail.com>
2022-06-07 14:08:55 +0530

    7-June-2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListStudentComponent from './components/ListStudentComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComponent/>
+         <div className="container">
+            <ListStudentComponent />
+          </div>
+          <FooterComponent/>
     </div>
   );
 }
