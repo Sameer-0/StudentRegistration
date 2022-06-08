02ceb95317fb7907fd42bf4a86e1d528529a5b99
Sameer-0 <sameershaikhx123@gmail.com>
2022-06-08 13:45:55 +0530

    June-8 12:30 PM

--- src/App.js
+++ src/App.js
@@ -12,15 +12,18 @@ function App() {
   return (
     <div className="App">
       <Router>
+
         <Navbar />
 
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/adduser" element={<AddUser />} />
-          <Route exact path="/edituser/:id" element={<EditUser />} />
-          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+           <Route exact path="/" element={<Home />} />
+           <Route exact path="/adduser" element={<AddUser />} />
+           <Route exact path="/edituser/:id" element={<EditUser />} />
+           <Route exact path="/viewuser/:id" element={<ViewUser />} />
         </Routes>
+
       </Router>
+
     </div>
   );
 }
--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -16,7 +16,7 @@ export default function ViewUser() {
   }, []);
 
   const loadUser = async () => {
-    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    const result = await axios.get(`https://spring-boot-hosting-v1.herokuapp.com/user/${id}`);
     setUser(result.data);
   };
 
